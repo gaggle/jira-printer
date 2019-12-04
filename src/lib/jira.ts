c2c68906157524eaf3d83d07e954e8afc0f4d9e0
Jon Lauridsen <mail@jonlauridsen.com>
2019-12-04 21:56:00 +0100

    Fix lint error

--- src/lib/jira.ts
+++ src/lib/jira.ts
@@ -1,9 +1,13 @@
 import { MockedParsedJiraBasicAuth, ParsedJiraBasicAuth } from 'simple-jira-client';
-import { ParsedEpic, ParsedIssue, ParsedStory } from 'simple-jira-client/build/types/ParsedIssue';
+import {
+  ParsedEpic as JiraEpic,
+  ParsedIssue as JiraIssue,
+  ParsedStory as JiraStory,
+} from 'simple-jira-client/build/types/ParsedIssue';
 import { Pingable, SimpleJiraClient } from 'simple-jira-client/src/types/index';
 import { Issue } from '../types/api-get-issues';
 
-let client: SimpleJiraClient<ParsedIssue, void> & Pingable | null;
+let client: SimpleJiraClient<JiraIssue, void> & Pingable | null;
 
 export function init(url: string, user: string, token: string, debug: boolean = false) {
   if (debug) {
@@ -21,15 +25,14 @@ export async function getIssues(issueKeys: string[]): Promise<Issue[]> {
   const result: Issue[] = [];
   for (const key of issueKeys) {
     console.log('Getting Jira issue', key);
-    const issue = await client.getIssue(key);
-
+    const issue: JiraIssue = await client.getIssue(key);
     switch (issue.issuetype.name) {
       case 'Epic':
-        result.push({ type: 'Epic', issue: issue as ParsedEpic });
+        result.push({ type: 'Epic', issue: issue as JiraEpic });
         break;
       case 'Story':
-        const epic = issue.epicKey ? await client.getIssue(issue.epicKey) as ParsedEpic : undefined;
-        result.push({ type: 'Story', issue: issue as ParsedStory, epic });
+        const epic = issue.epicKey ? await client.getIssue(issue.epicKey) as JiraEpic : undefined;
+        result.push({ type: 'Story', issue: issue as JiraStory, epic });
         break;
     }
   }
